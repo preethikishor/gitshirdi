b8f432f916d71e36da114299a73a2ba12c8f404e
preethikishor <30550110+preethikishor@users.noreply.github.com>
2019-03-12 23:39:40 +0530

    Update add.java

--- src/add.java
+++ src/add.java
@@ -4,8 +4,7 @@ public class add {
 	public static void main(String[] args) {
 		System.out.println("Exception while taking screenshotbbbdd ");
 		System.out.println("Exception while taking screenshot");
-		System.out.println("Exception while taking screenshotrrrr");
-		System.out.println("Exception while taking screenshotddd");
+		
 	}
 
 	}
